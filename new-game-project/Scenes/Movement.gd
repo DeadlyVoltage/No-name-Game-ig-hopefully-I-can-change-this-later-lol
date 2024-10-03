93703d323fe22a20273b39f80b389eb1c50ab389
VoltzzzWrld <chilitimez@proton.me>
2024-10-03 13:17:12 -0400

    Add files via upload

--- new-game-project/Scenes/Movement.gd
+++ new-game-project/Scenes/Movement.gd
@@ -1,4 +1,9 @@
 extends CharacterBody2D
- 
-var speed =  200
-var velocity = Vector2()
+
+var speed = 400  # speed in pixels/sec
+
+func _physics_process(delta):
+	var direction = Input.get_vector("PCmoveLeft", "PCmoveRight", "PCmoveUp", "PCmoveDown")
+	velocity = direction * speed
+
+	move_and_slide()
